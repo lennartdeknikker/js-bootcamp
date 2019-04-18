76239f06a3338baa653892e571f00eff875dace5
Lennart de Knikker <45396678+lennartdeknikker@users.noreply.github.com>
2019-04-18 14:13:51 +0200

    Solved the flattening excercise

--- l3-flattening.js
+++ l3-flattening.js
@@ -1,1 +1,8 @@
- 
+let arrays = [[1, 2, 3], [4, 5], [6]];
+const reducer = (accumulator, currentValue) => accumulator.concat(currentValue);
+
+function flatten2(arrs) {
+ arrs.reduce(reducer)
+}
+
+console.log(flatten(arrays));
