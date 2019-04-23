19ac044c0d7f4d5767475c307d8d5cf42c7e8400
Lennart de Knikker <45396678+lennartdeknikker@users.noreply.github.com>
2019-04-23 15:51:46 +0200

    Added my solution for "a Vector Type"

--- l3-vector-type.js
+++ l3-vector-type.js
@@ -1,1 +1,27 @@
- 
+ class Vec {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+   
+  get length() {
+    return Math.sqrt(this.x*this.x + this.y*this.y);
+  }
+  
+  plus(otherVector) {
+    let x = this.x + otherVector.x
+    let y = this.y + otherVector.y
+    return new Vec(x, y);
+  }
+   
+  minus(otherVector) {
+    return new Vec(this.x - otherVector.x, this.y - otherVector.y);
+  }
+}
+
+ console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+// → Vec{x: 3, y: 5}
+ console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+// → Vec{x: -1, y: -1}
+console.log(new Vec(3, 4).length);
+// → 5
