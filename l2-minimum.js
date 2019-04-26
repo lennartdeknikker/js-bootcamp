21514fa978984ec15b60025f69fe1acceab8fe33
Lennart de Knikker <45396678+lennartdeknikker@users.noreply.github.com>
2019-04-26 13:49:46 +0200

    Solved the minimum exercise

--- l2-minimum.js
+++ l2-minimum.js
@@ -1,1 +1,14 @@
- 
+ function min() {
+    let minimum = null;
+    for (i = 0; i < arguments.length; i++) {
+        if (minimum === null || minimum > arguments[i]) {
+            minimum = arguments[i];
+        }
+    }
+    return minimum;
+}
+
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
