8253c60ed4cb49ed0685b2442c76fd5772596b0b
Lennart de Knikker <45396678+lennartdeknikker@users.noreply.github.com>
2019-04-26 14:29:48 +0200

    solved the locked box

--- l5-the-locked-box.js
+++ l5-the-locked-box.js
@@ -1,1 +1,43 @@
- 
+ const box = {
+    locked: true,
+    unlock() {
+        this.locked = false;
+    },
+    lock() {
+        this.locked = true;
+    },
+    _content: [],
+    get content() {
+        if (this.locked) throw new Error("Locked!");
+        return this._content;
+    }
+};
+
+function withBoxUnlocked(body) {
+    let wasItLocked = box.locked;
+    if (box.locked === true) {
+        wasItLocked = true;
+        box.unlock();
+    }
+    try {
+        body();
+    } finally {
+        if (wasItLocked === true) {
+            box.lock();
+        }
+    }
+}
+
+withBoxUnlocked(function () {
+    box.content.push("gold piece");
+});
+
+try {
+    withBoxUnlocked(function () {
+        throw new Error("Pirates on the horizon! Abort!");
+    });
+} catch (e) {
+    console.log("Error raised:", e);
+}
+console.log(box.locked);
+// → true
