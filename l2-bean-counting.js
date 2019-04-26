13d1c419dc1a4a9ec2d43b2ed314d553f9abe5ba
Lennart de Knikker <45396678+lennartdeknikker@users.noreply.github.com>
2019-04-26 13:55:05 +0200

    Solved exercise Bean Counting

--- l2-bean-counting.js
+++ l2-bean-counting.js
@@ -1,1 +1,24 @@
- 
+ function countBs(string) {
+    let counter = 0;
+    for (i = 0; i <= string.length; i++) {
+        if (string[i] === 'B') {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+function countChar(string, char) {
+    let counter = 0;
+    for (i = 0; i <= string.length; i++) {
+        if (string[i] === char) {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4
