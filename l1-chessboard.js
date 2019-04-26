d69c3425f80d3ec2fb0284acdf9a5c6c4790f671
Lennart de Knikker <45396678+lennartdeknikker@users.noreply.github.com>
2019-04-26 12:30:32 +0200

    Solved Chessboard exercise

--- l1-chessboard.js
+++ l1-chessboard.js
@@ -1,1 +1,16 @@
+function createChessboard(height, width) {
+    let output = '';
+    for (h = 0; h < height; h++) {
+        for (w = 0; w < width; w++) {
+            if ((h % 2 + w) % 2 === 0) {
+                output += '#';
+            } else {
+                output += ' ';
+            }
+        }
+        output += '\n';
+    }
+    console.log(output);
+}
 
+createChessboard(8, 8);
