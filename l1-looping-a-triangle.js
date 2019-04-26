4ab3df014f04671be075da70c0ff80f213b0729d
Lennart de Knikker <45396678+lennartdeknikker@users.noreply.github.com>
2019-04-26 12:36:26 +0200

    solved looping a triangle

--- l1-looping-a-triangle.js
+++ l1-looping-a-triangle.js
@@ -1,1 +1,11 @@
+function triangle(height) {
+    for (i = 0; i <= height; i++) {
+        let line = '';
+        for (l = 0; l < i; l++) {
+            line += '#';
+        }
+        console.log(line);
+    }
+}
 
+triangle(7);
