f35d7814065c1e3a88a8d7a6ba4606b511aa4113
Lennart de Knikker <45396678+lennartdeknikker@users.noreply.github.com>
2019-04-26 12:06:27 +0200

    solved FizzBuzz excercise

--- l1-fizzbuzz.js
+++ l1-fizzbuzz.js
@@ -1,1 +1,18 @@
-r
+function fizzBuzz(lowest, highest) {
+    for (i = lowest; i <= highest; i++) {
+        if (i % 3 === 0) {
+            if (i % 5 === 0) {
+                console.log('FizzBuzz');
+            } else {
+                console.log('fizz');
+            }
+        } else if (i % 5 === 0) {
+            console.log('Buzz');
+        } else {
+            console.log('i');
+        }
+    }
+}
+
+
+fizzBuzz(1, 100);
